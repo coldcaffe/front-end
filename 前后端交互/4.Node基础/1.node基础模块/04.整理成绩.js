7166f18ab32a069d9e41ac1b6956a3e02086787c
gaoteng <358638369@qq.com>
2022-05-11 16:09:44 +0800

    node-fs

--- 前后端交互/4.Node基础/1.node基础模块/04.整理成绩.js
+++ 前后端交互/4.Node基础/1.node基础模块/04.整理成绩.js
@@ -2,7 +2,7 @@
 const fs = require('fs')
 
 // 2. 调用 fs.readFile() 读取文件的内容
-fs.readFile('../素材/成绩.txt', 'utf8', function(err, dataStr) {
+fs.readFile('../../素材/成绩.txt', 'utf8', function (err, dataStr) {
   // 3. 判断是否读取成功
   if (err) {
     return console.log('读取文件失败！' + err.message)
@@ -20,7 +20,7 @@ fs.readFile('../素材/成绩.txt', 'utf8', function(err, dataStr) {
   const newStr = arrNew.join('\r\n')
 
   // 5. 调用 fs.writeFile() 方法，把处理完毕的成绩，写入到新文件中
-  fs.writeFile('./files/成绩-ok.txt', newStr, function(err) {
+  fs.writeFile('./files/成绩-ok.txt', newStr, function (err) {
     if (err) {
       return console.log('写入文件失败！' + err.message)
     }
--- 前后端交互/4.Node基础/1.node基础模块/08.path.extname的使用.js
+++ 前后端交互/4.Node基础/1.node基础模块/08.path.extname的使用.js
@@ -2,6 +2,6 @@ const path = require('path')
 
 // 这是文件的存放路径
 const fpath = '/a/b/c/index.html'
-
+// 获取文件的扩展名
 const fext = path.extname(fpath)
 console.log(fext)
