a8eb3df0c71718423f89f2277b5021c6c048bc5f
gaoteng <358638369@qq.com>
2022-05-12 11:31:41 +0800

    补充提交

--- 前后端交互/4.Node基础/1.node基础模块/09.时钟案例.js
+++ 前后端交互/4.Node基础/1.node基础模块/09.时钟案例.js
@@ -8,7 +8,7 @@ const regStyle = /<style>[\s\S]*<\/style>/
 const regScript = /<script>[\s\S]*<\/script>/
 
 // 2.1 调用 fs.readFile() 方法读取文件
-fs.readFile(path.join(__dirname, '../素材/index.html'), 'utf8', function(err, dataStr) {
+fs.readFile(path.join(__dirname, '../素材/index.html'), 'utf8', function (err, dataStr) {
   // 2.2 读取 HTML 文件失败
   if (err) return console.log('读取HTML文件失败！' + err.message)
   // 2.3 读取文件成功后，调用对应的三个方法，分别拆解出 css, js, html 文件
@@ -24,7 +24,7 @@ function resolveCSS(htmlStr) {
   // 3.3 将提取出来的样式字符串，进行字符串的 replace 替换操作
   const newCSS = r1[0].replace('<style>', '').replace('</style>', '')
   // 3.4 调用 fs.writeFile() 方法，将提取的样式，写入到 clock 目录中 index.css 的文件里面
-  fs.writeFile(path.join(__dirname, './clock/index.css'), newCSS, function(err) {
+  fs.writeFile(path.join(__dirname, './clock/index.css'), newCSS, function (err) {
     if (err) return console.log('写入 CSS 样式失败！' + err.message)
     console.log('写入样式文件成功！')
   })
@@ -37,7 +37,7 @@ function resolveJS(htmlStr) {
   // 4.3 将提取出来的内容，做进一步的处理
   const newJS = r2[0].replace('<script>', '').replace('</script>', '')
   // 4.4 将处理的结果，写入到 clock 目录中的 index.js 文件里面
-  fs.writeFile(path.join(__dirname, './clock/index.js'), newJS, function(err) {
+  fs.writeFile(path.join(__dirname, './clock/index.js'), newJS, function (err) {
     if (err) return console.log('写入 JavaScript 脚本失败！' + err.message)
     console.log('写入 JS 脚本成功！')
   })
@@ -48,8 +48,10 @@ function resolveHTML(htmlStr) {
   // 5.2 将字符串调用 replace 方法，把内嵌的 style 和 script 标签，替换为外联的 link 和 script 标签
   const newHTML = htmlStr.replace(regStyle, '<link rel="stylesheet" href="./index.css" />').replace(regScript, '<script src="./index.js"></script>')
   // 5.3 写入 index.html 这个文件
-  fs.writeFile(path.join(__dirname, './clock/index.html'), newHTML, function(err) {
+  fs.writeFile(path.join(__dirname, './clock/index.html'), newHTML, function (err) {
     if (err) return console.log('写入 HTML 文件失败！' + err.message)
     console.log('写入 HTML 页面成功！')
   })
 }
+
+//此案例就是将css和js文件从html中拆分出来,分别写入对应文件中去
