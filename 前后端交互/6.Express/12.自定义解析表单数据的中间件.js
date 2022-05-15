8d2a4acf161cc2c301abb0d4a76bfb40d2eae669
gaoteng <358638369@qq.com>
2022-05-15 19:31:41 +0800

    cors与jsonp跨域

--- 前后端交互/6.Express/12.自定义解析表单数据的中间件.js
+++ 前后端交互/6.Express/12.自定义解析表单数据的中间件.js
@@ -2,7 +2,7 @@
 const express = require('express')
 // 创建 express 的服务器实例
 const app = express()
-// 导入 Node.js 内置的 querystring 模块
+// 导入 Node.js 内置的 querystring 模块,专用于处理查询字符串
 const qs = require('querystring')
 
 // 这是解析表单数据的中间件
@@ -11,6 +11,8 @@ app.use((req, res, next) => {
   // 1. 定义一个 str 字符串，专门用来存储客户端发送过来的请求体数据
   let str = ''
   // 2. 监听 req 的 data 事件
+  //如果数据量较大,无法一次性发送完毕,则客户端会把数据切割后分批发送到服务器,所以data事件可能会触发多次
+  //每次触发data事件时,获取到数据只是完整数据的一部分,需要手动对接收到的数据进行拼接;
   req.on('data', (chunk) => {
     str += chunk
   })
